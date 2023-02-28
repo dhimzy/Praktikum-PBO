b2f2a86c0fac9597046e035b0cb9b947618a58e3
dhimzy <100180259+dhimzy@users.noreply.github.com>
2023-02-28 21:16:13 +0700

    Add files via upload

---
+++ Praktikum 1/MTitik.java
@@ -0,0 +1,31 @@
+/************************************/
+/* Nama File : MTitik.java */
+/* Deskripsi : Kelas yang berisi program utama yang memanfaatkan kelas Titik */
+/* NIM/Nama  : 24060121140151/Dhiya Mazaya */
+/* Tanggal   : 22 Februari 2023 */
+/***********************************/
+
+class MTitik {
+	public static void main(String[] args) {
+		Titik t1;
+		Titik t2;
+		Titik t3;
+		t1 = new Titik();
+		t2 = new Titik();
+		t3 = new Titik();
+		
+		t1.setAbsis(1.0);
+		t1.setOrdinat(2.0);
+		
+		t2.setAbsis(3.0);
+		t2.setOrdinat(4.0);
+
+		t3.setAbsis(5.0);
+		t3.setOrdinat(6.0);
+		
+		System.out.println("Jumlah objek titik : "+t1.getCounterTitik());
+		System.out.println("t1("+t1.getAbsis()+","+t1.getOrdinat()+")");
+		System.out.println("t2("+t2.getAbsis()+","+t2.getOrdinat()+")");
+		System.out.println("t3("+t3.getAbsis()+","+t3.getOrdinat()+")");
+	}
+}
---
+++ Praktikum 1/Titik.java
@@ -0,0 +1,44 @@
+/************************************/
+/* Nama File : Titik.java */
+/* Deskripsi : Program utama yang memanfaatkan OperasiTitik */
+/* NIM/Nama  : 24060121140151/Dhiya Mazaya */
+/* Tanggal   : 22 Februari 2023 */
+/***********************************/
+
+class Titik{
+	double absis; 
+	double ordinat;
+	static int counterTitik;
+
+	Titik(){
+		counterTitik++;
+		absis = 0;
+		ordinat = 0;
+	}
+
+	Titik(int a, int b){
+		counterTitik++;
+		absis = a;
+		ordinat = b;
+	}
+
+	void setAbsis(double a){
+		absis =a;
+	}
+
+	void setOrdinat(double o){
+		ordinat = o;
+	}
+
+	double getAbsis(){
+		return absis;
+	}
+
+	double getOrdinat(){
+		return ordinat;
+	}
+
+	double getCounterTitik(){
+		return counterTitik;
+	}
+}
